d995b7e0dc3320bcee4f830fa25d8cd9459ce763
Aaron Leung <aaronleung726@hotmail.com>
2019-06-03 02:08:00 -0600

    Implemented functionality to update user profiles

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm (UserCreationForm):
     email = forms.EmailField()
@@ -11,4 +12,16 @@ class UserRegisterForm (UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
+class UserUpdateForm (forms.ModelForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length = 30)
+    last_name = forms.CharField(max_length = 30)
 
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+class ProfileUpdateForm (forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
